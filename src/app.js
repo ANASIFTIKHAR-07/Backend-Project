ec07a39109ea1ab2dcd1c9c62ccfeb04925ccaab
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-01 23:01:35 +0500

    Controller add with route setup for register

--- src/app.js
+++ src/app.js
@@ -11,13 +11,22 @@ app.use(cors(
         credentials: true,
     }
 ))
-// The (use) keyword is used for all the middlewares and the configurations 
+// The (use) keyword is used for all the middlewares and the configurations  
 // Express configuration for different data
 app.use(express.json({limit: "16kb"})) // For json format
 app.use(express.urlencoded({limit: "16kb", extended: true,})) // For the data which comes from the URL
 app.use(express.static("public"))// For storing some pdf, images, create a public folder that is accessible
 // Already created a public folder in which the files would be stored
 app.use(cookieParser())
+
+
+// Routes import
+
+import userRouter from './routes/user.routes.js'
+
+// Routes declaration
+
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
