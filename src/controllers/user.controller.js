b9ce5e1e35d92fa59e20107e2d86a3346b5997eb
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-06 14:09:58 +0500

    Returning the user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,8 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
-
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req, res)=> {
     const {fullName, password, userName, email} = req.body
@@ -34,15 +33,23 @@ const registerUser = asyncHandler( async (req, res)=> {
      if (!avatar) {
         throw new ApiError(400, "Avatar is required")
      }
-     User.create({
+     const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage.url || "",
         email,
         password,
         userName : userName.toLowerCase()
-
      })
+     const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+     )
+     if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering user")
+     }
+     return res.status(201).json(
+        new ApiResponse(200, createdUser, "User is regiestered Successfully")
+     )
 })
 
 
