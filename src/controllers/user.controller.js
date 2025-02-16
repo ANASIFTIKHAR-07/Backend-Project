db79f6151781da1f8f20ac4f484a4d235d96e728
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-16 17:12:07 +0500

    Updating the login function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,8 +10,11 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 const addAccessAndRefereshToken = async(userId) => {
     try {
         const user = await User.findById(userId)
-          const accessToken = user.generateAccessToken
-          const refreshToken = user.generateRefreshToken
+          const accessToken = user.generateAccessToken()
+          const refreshToken = user.generateRefreshToken()
+          user.refreshToken = refreshToken
+          await user.save({validateBeforeSave: false})
+          return {accessToken, refreshToken}
     } catch (error) {
         throw new ApiError(500, "Something went wrong while generating Access and referesh tokens!")
     }
@@ -99,6 +102,10 @@ const loginUser = asyncHandler(async (req, res)=> {
     if (!isPasswordValid) {
         throw new ApiError(401, "Password is invalid")
     }
+
+    const {accessToken, refreshToken} = await addAccessAndRefereshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 })
 
 export { 
