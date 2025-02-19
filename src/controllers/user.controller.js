4e823fdab1ee61c21adb9b6fdfb89453b504b8a9
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-19 15:23:43 +0500

    Further addition of Update functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import { url } from "inspector"
 
 
 
@@ -253,6 +254,64 @@ const updateAccountDetails = asyncHandler(async(req, res)=> {
         .json(new ApiResponse(200 , user, "Account details updated successfully"))
 })
 
+const updateUserAvatar = asyncHandler(async(req, res)=> {
+      const avatarLocalPath =  req?.file.path
+
+      if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing")
+      }
+
+      const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+      if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+      }
+
+      const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+      ).select("-password")
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res)=> {
+    const coverImageLocalPath = req?.file.path
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image is missing")
+    }
+
+    const coverImage =  await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage-url) {
+        throw new ApiError(400, "Error while uploading on Cover Image!")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url, 
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
 export { 
     registerUser,
     loginUser,
@@ -260,4 +319,7 @@ export {
     accessRefreshToken,
     changeCurrentPassword,
     getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
 } 
