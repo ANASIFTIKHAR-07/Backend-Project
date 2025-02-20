9e615c447b9f9e03c217a9c6cf29e0598421bde3
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-20 19:18:51 +0500

    ApiResponse debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -222,14 +222,22 @@ const changeCurrentPassword = asyncHandler(async (req, res)=> {
 
         return res
         .status(200)
-        .json(200, {}, "Password updated successfully")
+        .json(new ApiResponse(
+            200,
+            {},
+            "Password updated successfully"
+        ))
 
 })
 
 const getCurrentUser = asyncHandler(async(req, res)=> {
     return res
     .status(200)
-    .json(200, req.user, "Current user fetched successfully")
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User Fetched successfulle"
+    ))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res)=> {
