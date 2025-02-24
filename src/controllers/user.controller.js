a48b63126db386b67d9082d3caa59fd973452a8b
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-24 21:46:06 +0500

    Updation Pipeline complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -442,6 +442,16 @@ const getWatchHistory = asyncHandler(async(req, res)=> {
                 }
               }
         ])
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].getWatchHistory,
+                "Watch History Fetched Successfully"
+            )
+        )
 })
 export { 
     registerUser,
