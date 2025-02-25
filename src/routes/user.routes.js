a6479148c0522fb04893a2e709ff60d893407b00
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-02-25 21:14:35 +0500

    Slight changing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ import {
        updateUserCoverImage,
        getUserChannelProfile,
        getWatchHistory
-    } from '../controllers/user.controller.js';
+} from '../controllers/user.controller.js';
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -43,7 +43,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("update-datails").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
